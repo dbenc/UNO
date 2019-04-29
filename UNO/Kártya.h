9d22034dab581c7acac4c911d947f742b0052357
Dajka Bence <dajka.bence2@gmail.com>
2019-04-29 21:33:23 +0200

    Kártya header

---
+++ UNO/Kártya.h
@@ -0,0 +1,33 @@
+﻿#ifndef _CARD_H_
+#define _CARD_H_
+
+#include <ostream>
+
+
+enum COLOR { wild, red, green, blue, yellow };
+
+
+class card
+{
+public:
+	int number; // 0-9 a számok, +2, kirekesztő, retúr, színválasztás, +4 (színválasztás) 
+	COLOR color; // 5 szín: síma, zöld, kék, sárga, piros 
+
+	//Egyenlőség operátor
+	// Ekkor lerakható a kártya a körben
+	bool operator==(card const& other) const;
+
+	// Egyenlőtlenség operátor
+	bool operator!=(card const& other) const;
+
+
+	card();
+
+	card(int num, COLOR col);
+};
+
+
+std::ostream& operator<<(std::ostream& out, card const& temp_card);
+
+
+#endif // _CARD_H_
